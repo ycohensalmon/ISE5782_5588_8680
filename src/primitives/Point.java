3bdbcd6976dc631108ba4407c15b587f4d67c4ab
אלחנן טוויג <tweig@g.jct.ac.il>
2022-03-02 21:25:00 +0200

    order

---
+++ src/primitives/Point.java
@@ -0,0 +1,16 @@
+package primitives;
+
+public class Point {
+
+    public Point(int i, int j, int k) {
+    }
+
+    public Object subtract(Point p1) {
+        return null;
+    }
+
+    public Object add(Vector vector) {
+        return null;
+    }
+
+}
