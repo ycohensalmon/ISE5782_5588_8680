8a797a7377969192a9aed8a0d2b592a9353046e9
אלחנן טוויג <tweig@g.jct.ac.il>
2022-03-22 19:52:17 +0200

    findIntsersections

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -3,6 +3,16 @@ package geometries;
 import primitives.*;
 import java.util.List;
 
-public interface Intersectable {
-    public List findIntsersections(Ray ray);
+/**
+ * Common interface for all graphic objects
+ * that intersect with a ray{@link Ray}
+ */
+public interface Intersectable{
+    /**
+     * find all intersection points {@link Point}
+     * that intersect with a specific ray{@link Ray}
+     * @param ray ray pointing towards the graphic object
+     * @return immutable list of intersection points {@link Point}
+     */
+    List<Point> findIntsersections(Ray ray);
 }
