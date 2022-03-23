d2b61eecf98048b80ecb21edcb503ef642c27810
אלחנן טוויג <tweig@g.jct.ac.il>
2022-03-23 22:30:44 +0200

    findIntsersections Triangle

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -2,8 +2,11 @@ package geometries;
 
 import primitives.Point;
 import primitives.Ray;
+import primitives.Vector;
 
 import java.util.List;
+
+import static primitives.Util.alignZero;
 
 /**
  * this class represents a Triangle
@@ -19,8 +22,40 @@ public class Triangle extends Polygon {
         super(p1, p2, p3);
     }
 
+
+    /**
+     * find all intersection points {@link Point}
+     * that intersect with a specific ray{@link Ray}
+     * @param ray ray pointing towards the triangle
+     * @return immutable list of intersection points {@link Point}
+     */
     @Override
     public List<Point> findIntersections(Ray ray) {
-        return super.findIntersections(ray);
+        List<Point> result = plane.findIntersections(ray);
+
+        //Check if the ray intersect the plane.
+        if (result == null) {
+            return null;
+        }
+
+        Vector v1 = vertices.get(0).subtract(ray.getP0());
+        Vector v2 = vertices.get(1).subtract(ray.getP0());
+        Vector v3 = vertices.get(2).subtract(ray.getP0());
+
+        Vector n1 = v1.crossProduct(v2).normalize();
+        Vector n2 = v2.crossProduct(v3).normalize();
+        Vector n3 = v3.crossProduct(v1).normalize();
+
+        Vector v = ray.getDir();
+
+        double vn1 = alignZero(v.dotProduct(n1));
+        double vn2 = alignZero(v.dotProduct(n2));
+        double vn3 = alignZero(v.dotProduct(n3));
+
+        //The point is inside if all 𝒗 ∙ 𝒏𝒊 have the same sign (+/-)
+        if ((vn1 > 0 && vn2 > 0 && vn3 > 0) || (vn1 < 0 && vn2 < 0 && vn3 < 0)) {
+            return result;
+        }
+        return null;
     }
 }
